34c4d26187a7d3f8327c3229c1a6fd28fa8ac061
Ferencz Tamas <tamas@static-172-17-0-16.rdsnet.ro>
2020-11-27 08:39:31 +0200

    Hazi feladat egyszerusitve

---
+++ 2020.11.20.Hf.Maskepp.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    char s[102], ut;
+    int i = strlen(s) - 1, x;
+
+    cout << "s=";
+    cin.getline(s, 102);
+
+    while (i != 0)
+    {
+        while (s[i] != ' ' && s[i] != '\0')
+        {
+
+            ut = s[i];
+        }
+        i--;
+    }
+    cout << "s= " << s << endl;
+
+    return 0;
+}
