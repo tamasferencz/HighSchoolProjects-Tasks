f91cce84753eb80e1da23525de355755f160d008
Ferencz Tamas <tamas@flbs-MacBook-Pro.local>
2021-02-10 10:11:48 +0200

    Hazi modositasok

--- HF.2020.02.09.cpp
+++ HF.2020.02.09.cpp
@@ -9,52 +9,70 @@ using namespace std;
 
 using namespace std;
 
-struct tanulo {
+struct tanulo
+{
     string nev;
-    int jegy1, jegy2, jegy3, jegy4;
+    float jegy1, jegy2, jegy3, jegy4;
     int dolgozat;
-    int media;
+    float media;
 };
-
 
 int main()
 {
-    int n,i;
+    int n, i, z;
+    float atlag, y;
     cout << "Add meg az n-t: ";
     cin >> n;
 
     cin.get();
-       
+
     tanulo tan[n];
 
-    for( i=0;i<n;i++){
-
-    cout << "A tanulo neve: ";
-    getline(cin, tan[i].nev);
-    cout << "A tanulo 1. jegye: "; cin >> tan[i].jegy1;
-    cout << "A tanulo 2. jegye: "; cin >> tan[i].jegy2;
-    cout << "A tanulo 3. jegye: "; cin >> tan[i].jegy3;
-    cout << "A tanulo 4. jegye: "; cin >> tan[i].jegy4;
-    cout << "A tanulo dolgozata: "; cin >> tan[i].dolgozat; 
-    cin.get();
-    }
-    
-    for(i = 0; i < n; i++)
+    for (i = 0; i < n; i++)
     {
-        cout << "A tanulo neve: " << tan[i].nev << endl;
-        tan[i].media = ((((tan[i].jegy1 + tan[i].jegy2 + tan[i].jegy3 + tan[i].jegy4)/4)*3)+tan[i].dolgozat)/4;
+
+        cout << "A tanulo neve: ";
+        getline(cin, tan[i].nev);
+        cout << "A tanulo 1. jegye: ";
+        cin >> tan[i].jegy1;
+        cout << "A tanulo 2. jegye: ";
+        cin >> tan[i].jegy2;
+        cout << "A tanulo 3. jegye: ";
+        cin >> tan[i].jegy3;
+        cout << "A tanulo 4. jegye: ";
+        cin >> tan[i].jegy4;
+        cout << "A tanulo dolgozata: ";
+        cin >> tan[i].dolgozat;
+        cin.get();
+        tan[i].media = ((((tan[i].jegy1 + tan[i].jegy2 + tan[i].jegy3 + tan[i].jegy4) / 4) * 3) + tan[i].dolgozat) / 4;
         cout << "A tanulo mediaja: " << tan[i].media << endl;
     }
 
-    for(i = 0; i < n; i++)
+    for (i = 0; i < n; i++)
     {
-        if(tan[i].media<5)
+        if (tan[i].media < 5)
         {
             cout << "Ez a tanulo megbukott: " << tan[i].nev << endl;
         }
+    }
 
+    for (i = 0; i < n; i++)
+    {
+        z++;
+        y += tan[i].media;
+    }
+
+    atlag = y / z;
+
+    cout << "Osztaly atlag: " << atlag << endl;
+
+    for (i = 0; i < n; i++)
+    {
+        if (tan[i].media > atlag)
+        {
+            cout << "Ennek a diaknak a mediaja jobb, mint az osztalyatlag: " << tan[i].nev << endl;
+        }
     }
 
     return 0;
-    
 }
